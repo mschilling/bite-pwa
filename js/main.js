93b0380470d7c12967a3331ffd9bf8f964c95df4
jstrating <jesper.strating@outlook.com>
2016-10-10 14:52:21 +0200

    Clean pwa, build fix :gnome:

--- js/main.js
+++
@@ -1,2 +0,0 @@
-function addBite(){$(".bite-cards").append("<bite-card name='PRIMATARIA' background='url(../../images/harambe.jpg)' desc='🌯🌯🌯🌯🌯' location='Gramsbergen' people=''></bite-card>");
-     }
