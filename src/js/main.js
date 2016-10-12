7a4f4cb5bfea7a3de86b24c1b297ac88fac4ee8b
jstrating <jesper.strating@outlook.com>
2016-10-12 16:23:44 +0200

    Main screen, no build errors :megusta:

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+function addBite(){$(".bite-cards").append("<bite-card name='PRIMATARIA' background= 'url(../images/harambe.jpg)' desc='🌯🌯🌯🌯🌯' location='Gramsbergen'></bite-card>");
+    console.log("hoi");
+}
