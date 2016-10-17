3ba3071311995b8796bc9be5192ff5530ae647de
jstrating <jesper.strating@outlook.com>
2016-10-17 08:42:39 +0200

    Page navigatie, dynamic header change

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 function addBite(){$(".bite-cards").append("<bite-card name='PRIMATARIA' background= 'url(../images/harambe.jpg)' desc='🌯🌯🌯🌯🌯' location='Gramsbergen'></bite-card>");
     console.log("hoi");
 }
+
+
+
