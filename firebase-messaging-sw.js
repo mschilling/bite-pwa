862e0b7e5db0300ff8818cc4d4ae38c3c8831269
jstrating <jesper.strating@outlook.com>
2016-12-09 14:40:51 +0100

    Service worker/Firebase messaging sw, basic push notifications with click event

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,46 @@
+importScripts('https://www.gstatic.com/firebasejs/3.6.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  'messagingSenderId': '389337808613'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.data.title;
+  const notificationOptions = {
+    body: payload.data.message,
+    icon: payload.data.image_url,
+    data: {
+      bite: payload.data.bite
+    }
+  };
+
+  self.addEventListener('notificationclick', function(event){
+    event.notification.close();
+    // Check if the current is already open
+    event.waitUntil(
+      clients.matchAll({
+        type: "window"
+      }).then(function(clientList){
+        for(var i = 0; i < clientList.length; i++){
+          var client = clientList[i];
+          if(client.url == '/' && 'focus' in client)
+            return client.focus();
+        }
+        if(clients.openWindow){
+          return clients.openWindow('http://localhost:8080/join/'+event.notification.data.bite);
+        }
+      })
+    )
+  });
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+
+
+});
+// [END background_handler]
